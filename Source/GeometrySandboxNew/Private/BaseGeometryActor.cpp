ae64ef1ae71f5768648664678a1ebddd11b5f1c4
men325 <333322224ik@gmail.com>
2023-07-24 20:43:38 +0300

    Update BaseGeometryActor.cpp

--- Source/GeometrySandboxNew/Private/BaseGeometryActor.cpp
+++ Source/GeometrySandboxNew/Private/BaseGeometryActor.cpp
@@ -3,6 +3,7 @@
 
 #include "BaseGeometryActor.h"
 #include "Engine/Engine.h"
+#include "Materials/MaterialInstanceDynamic.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogBaseGeometry, All, All)
 
@@ -23,10 +24,11 @@ void ABaseGeometryActor::BeginPlay()
 
     InitialLocation = GetActorLocation();
 
-    //printStringTypes();
-    // printTypes();
-    //printTransform();
-    
+    //PrintStringTypes();
+    //PrintTypes();
+    //PrintTransform();
+
+    SetColor(GeometryData.Color);
 }
 
 // Called every frame
@@ -34,16 +36,38 @@ void ABaseGeometryActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-    // z = z0 + amplitude * sin(freq * t);
-    FVector CurrentLocation = GetActorLocation();
-    float time = GetWorld()->GetTimeSeconds();
-    CurrentLocation.Z = InitialLocation.Z + Amplitude * FMath::Sin(Frequency * time);
-    SetActorLocation(CurrentLocation);
-
+    HandleMovement();
 
 }
 
-void ABaseGeometryActor::printTypes()
+void ABaseGeometryActor::HandleMovement()
+{
+    switch (GeometryData.MoveType)
+    {
+    case EMovementType::Sin:
+    {
+        // z = z0 + amplitude * sin(freq * t);
+        FVector CurrentLocation = GetActorLocation();
+        float Time = GetWorld()->GetTimeSeconds();
+        CurrentLocation.Z = InitialLocation.Z + GeometryData.Amplitude * FMath::Sin(GeometryData.Frequency * Time);
+        SetActorLocation(CurrentLocation);
+    }
+    break;
+    case EMovementType::Static: break;
+    default: break;
+    }
+}
+
+void ABaseGeometryActor::SetColor(const FLinearColor& Color)
+{
+    UMaterialInstanceDynamic* DynMaterial = BaseMesh->CreateAndSetMaterialInstanceDynamic(0);
+    if (DynMaterial)
+    {
+        DynMaterial->SetVectorParameterValue("Color", Color);
+    }
+}
+
+void ABaseGeometryActor::PrintTypes()
 {
     UE_LOG(LogTemp, Display, TEXT("Weapons num: %d, kills num: %i"), WeaponsNum, KillsNum);
     UE_LOG(LogTemp, Display, TEXT("Health: %f"), Health);
@@ -52,7 +76,7 @@ void ABaseGeometryActor::printTypes()
     UE_LOG(LogTemp, Display, TEXT("HasWeapon: %d"), static_cast<int>(HasWeapon));
 }
 
-void ABaseGeometryActor::printStringTypes()
+void ABaseGeometryActor::PrintStringTypes()
 {
     FString Name = "John Connor";
     UE_LOG(LogBaseGeometry, Display, TEXT("Name: %s"), *Name);
@@ -68,7 +92,7 @@ void ABaseGeometryActor::printStringTypes()
     GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, Stat, true, FVector2D(1.5f, 1.5f));
 }
 
-void ABaseGeometryActor::printTransform() 
+void ABaseGeometryActor::PrintTransform() 
 {
     FTransform Transform = GetActorTransform();
     FVector Location = Transform.GetLocation();
